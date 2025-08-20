ee87d895994ae50615ba6de98ceb58e78a9c10d0
Aman <rajaman9355@gmail.com>
2025-08-20 12:13:37 +0530

    Added Student Attendence APIs

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -2,8 +2,7 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import StudentHome from './StudentHome'
-
+import StudentHome from "./StudentHome";
 
 interface User {
   sub: string;
@@ -13,33 +12,47 @@ interface User {
   designation?: string;
 }
 
-const page = () => {
-    const [user, setUser] = useState<User | null>(null);
+const Page = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [userDetails, setUserDetails] = useState<User | null>(null);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/me`, {
-          withCredentials: true, 
-        });
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/me`,
+          { withCredentials: true }
+        );
 
-        const userData = res.data.data.user;
-
+        const userData: User = res.data.data.user;
         localStorage.setItem("user", JSON.stringify(userData));
-
         setUser(userData);
+
+        fetchUserDetails(userData.sub);
       } catch (err) {
         console.error("Error fetching user:", err);
+      }
+    };
+
+    const fetchUserDetails = async (userId: string) => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/students-profile/${userId}/basic-details`,
+          { withCredentials: true }
+        );
+
+        const userDetailsData = res.data.data;
+        localStorage.setItem("userDetails", JSON.stringify(userDetailsData));
+        setUserDetails(userDetailsData);
+      } catch (err) {
+        console.error("Error fetching user details:", err);
       }
     };
 
     fetchUser();
   }, []);
 
-  return (
-    <StudentHome />
-  )
-}
+  return <StudentHome />;
+};
 
-export default page
-
+export default Page;
