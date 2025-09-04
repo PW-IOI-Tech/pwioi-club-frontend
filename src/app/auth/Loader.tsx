b055e081af4d76e043eef4b0ce4ff54da45ad79d
nishb <nishbcodes@gmail.com>
2025-09-04 14:04:11 +0530

    Loader Colors Changed

--- src/app/auth/Loader.tsx
+++ src/app/auth/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-50">
-      <div className="w-20 h-20 border-6 border-t-slate-900 border-r-transparent border-b-slate-400 border-l-transparent rounded-full animate-spin"></div>
+      <div className="w-20 h-20 border-6 border-t-[#12294c] border-r-transparent border-b-[#4d6d9e] border-l-transparent rounded-full animate-spin"></div>
 
       <p className="text-slate-600 text-sm mt-6 font-medium">
         Authenticating...
